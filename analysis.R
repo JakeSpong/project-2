5cd8a5a394ec0c546d946407de1029e5804847e1
JakeSpong <jake.f.spong@york.ac.uk>
2024-10-01 17:36:15 +0100

    mapping sample points, veg beta diversity

---
+++ analysis.R
@@ -0,0 +1,91 @@
+usethis::use_git()
+
+## Setup ----
+# Load libraries
+library(dplyr) #for manipulating data
+library(ggplot2) #for plotting graphs
+library(ggpubr)
+library(vegan) #for NMDS
+library(tidyverse)
+library(car) #for levene test
+library(dataspice) #for creating metadata files
+library(multcompView) #for significant difference letters
+
+#### Plot a map of our sample locations ----
+
+library(leaflet)
+library(googleVis)
+
+#load the field data
+individual <- readr::read_csv(
+  here::here("data", "all-sites_field-data.csv"), show_col_types = FALSE
+) 
+
+# Using leaflet - dynamic map
+# Sites projected onto an Open Street Map background
+# leaflet uses the viewer panel to display its output.
+# Works when called from RStudio but no longer from the 
+# R console.
+
+
+background <- addTiles(leaflet())
+mymap1 <-
+  addMarkers(
+    background,
+    lng = individual$LongitudeE,
+    lat = individual$LatitudeN,
+    label = as.character(individual$`SampleID`),
+    labelOptions = labelOptions(noHide = TRUE, textOnly = FALSE)
+  )
+#map of our sampling locations
+mymap1
+
+#### Beta diversity analysis of vegetation survey data ----
+#### Load vegetation abundance data
+d <- readr::read_csv(
+  here::here("data", "all-sites_field-data.csv"), show_col_types = FALSE
+) 
+#order samples by ID alphabetically
+d <- arrange(d, d["Sample ID"])
+d <- as.data.frame(d)
+#replace row index with sample names
+#rownames(d) <- c("GB1", "GB2", "GB3", "GB4", "GB5", "GN10", "GN3", "GN4", "GN5","GN9", "UB1","UB10","UB4","UB6", "UB9", "UN1", "UN10", "UN3", "UN7", "UN8", "WB3","WB4","WB6","WB7","WB8","WN2","WN4","WN5","WN6","WN8")
+#replace null (empty excell cell) with "0"
+d[is.na(d)] <- 0
+#make sure our variables are coded as factors
+d$Land <- factor(d$Land, levels = c("Bracken", "Heather"), labels = c("Bracken", "Heather"))
+#just the species counts
+spe <- d[,-(1:5)]
+spe <- spe[,-(13:14)]
+
+#k is the number of reduced dimensions
+#trymax sets the default number of iterations
+example_NMDS <- metaMDS(spe, distance = "bray", k = 2, maxit = 999, trymax = 500)
+#Shephard plot shows scatter around the regession between the interpoint distances in the final configuration (i.e. the distances between each pair of communities) against their original dissimilarities.  Large scatter around the line suggests the original dissimilarities are not well preserved in the reduced number of dimensions
+stressplot(example_NMDS)
+
+#add extra space to the right of the plot
+par(mar=c(5, 4, 4, 12), xpd=TRUE)
+#plot the NMDS
+plot(example_NMDS, col = "white")
+#change width of axes and surroundng box
+axis(side = 1, lwd = 2)
+axis(side = 2, lwd = 2)
+box(lwd = 2)
+
+#assign the treatments to relevant rows of the dataframe
+treat=c(rep("Bracken",50),rep("Heather",50))
+#set the colour for each treatment
+colors=c(rep("#117733",50), rep("#AA4499", 50))
+text(-1.2,1.3, paste("Stress = ", round(example_NMDS$stress, 3)))
+
+for(i in unique(treat)) {
+  #we have added an if statement so we can chose which points and ellipses to plot at a time e.g. i == "Grassland Bracken".  If we want to plot all ellipses simultaneously, set i == i
+  if(i == i){
+    #change the colour of each site name so samples from the s  Eame treatment have the same colour
+    orditorp(example_NMDS$point[grep(i,treat),],display="sites", col=colors, cex=0.7,air=0.01)
+    #plots ellipse with ellipse centered on the centroid of the samples from the same treatment (and thus encapsulating 95% of the variance)
+    ordiellipse(example_NMDS$point[grep(i,treat),],draw="polygon",
+                groups=treat[treat==i],col=colors[grep(i,treat)],label=F) } }
+#specify legend manually
+legend(2,0.6, legend = c("Grassland Bracken", "Grassland Non-bracken", "Heathland Bracken", "Heathland Non-bracken", "Woodland Bracken", "Woodland Non-bracken"), fill = c("#44AA99", "#117733", "#88CCEE", "#332288", "#AA4499"))
